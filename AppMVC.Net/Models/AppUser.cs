aaa5b70bb42cb060f224857d22badc45106b6c20
nhannt123 <nhankhangg@gmail.com>
2022-06-06 16:23:59 +0700

    abc

---
+++ AppMVC.Net/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppMVC.Net.Models
+{
+    public class AppUser: IdentityUser
+    {
+        [Column(TypeName = "nvarchar(400)")]
+        public string HomeAdress { get; set; }
+
+        // [Required]       
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+    }
+}
---
+++ AppMVC.Net/Models/PaggingModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppMVC.Net.Models
+{
+    public class PaggingModel
+    {
+        public int currentpage { get; set; }
+        public int countpages { get; set; }
+
+        public Func<int?, string> generateUrl { get; set; }
+    }
+}
